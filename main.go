122804f79bad228618c8e14bb827ffd38befff65
marufboy <afif23ixa@gmail.com>
2023-02-15 15:24:59 +0800

    feat: swagger docs to REST API

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	_ "github.com/marufboy/golang-rest-api-postgres/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/marufboy/golang-rest-api-postgres/config"
 	"github.com/marufboy/golang-rest-api-postgres/controllers"
 	"github.com/marufboy/golang-rest-api-postgres/routes"
-	// "github.com/marufboy/golang-rest-api-postgres/routes"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -24,7 +27,7 @@ var (
 func init() {
 	setup, err := config.LoadConfig(".")
 	if err != nil {
-		log.Fatal("🚀 Could not load environment variables", err)
+		log.Fatal().Err(err).Msg("🚀 Could not load environment variables")
 	}
 
 	config.ConnectDB(&setup)
@@ -36,15 +39,28 @@ func init() {
 	UserRouteController = routes.NewUserRouteController(UserController)
 
 	//initialize gin
-	// gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 	// server = gin.New() //empty engine
 	server = gin.Default() //empty engine
 }
 
+// @title 		Gin User Auth Service
+// @version 	1.0
+// @description A User Auth Service API in Go using Gin framework
+
+// @contact.name   Muhammad Afif Ma'ruf
+// @contact.url    http://www.swagger.io/support
+// @contact.email  afif23ixa@gmail.com
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host 		localhost:8000
+// @BasePath 	/api
 func main() {
 	setup, err := config.LoadConfig(".")
 	if err != nil {
-		log.Fatal("🚀 Could not load environment variables", err)
+		log.Fatal().Err(err).Msg("🚀 Could not load environment variables")
 	}
 
 	//cors setup
@@ -57,6 +73,9 @@ func main() {
 	// server.Use(middleware.DefaultStructuredLogger()) //adds our new middleware logger
 	// server.Use(gin.Recovery())
 
+	//config swagger to router
+	server.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
 		message := "Welcome to Golang with Gorm and Postgres"
@@ -66,5 +85,6 @@ func main() {
 	AuthRouteController.AuthRoute(router)
 	UserRouteController.UserRoute(router)
 
-	log.Fatal(server.Run(":" + setup.ServerPort))
+	err = server.Run(":" + setup.ServerPort)
+	log.Fatal().Err(err).Msg("🚀 Could not run the server")
 }
